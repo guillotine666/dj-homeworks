78a369cf9897628475363fab2a11e6b30c4ccdb9
mMatvey <xaxalox2@gmail.com>
2019-10-04 22:54:14 +0500

    add must have tasks

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -2,33 +2,38 @@ from collections import Counter
 
 from django.shortcuts import render_to_response
 
-# Для отладки механизма ab-тестирования используйте эти счетчики
-# в качестве хранилища количества показов и количества переходов.
-# но помните, что в реальных проектах так не стоит делать
-# так как при перезапуске приложения они обнулятся
-counter_show = Counter()
-counter_click = Counter()
+
+counter_show = Counter(original=0, alternative=0)
+counter_click = Counter(original=0, alternative=0)
 
 
 def index(request):
-    # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    from_landing = request.GET.get('from-landing')
+    if from_landing == 'original':
+        counter_click['original'] += 1
+    elif from_landing == 'test':
+        counter_click['alternative'] += 1
     return render_to_response('index.html')
 
 
 def landing(request):
-    # Реализуйте дополнительное отображение по шаблону app/landing_alternate.html
-    # в зависимости от GET параметра ab-test-arg
-    # который может принимать значения original и test
-    # Так же реализуйте логику подсчета количества показов
-    return render_to_response('landing.html')
+    ab_arg = request.GET.get('ab-test-arg')
+    if ab_arg == 'original':
+        counter_show['original'] += 1
+        return render_to_response('landing.html')
+    elif ab_arg == 'test':
+        counter_show['alternative'] += 1
+        return render_to_response('landing_alternate.html')
+    else:
+        return render_to_response('landing.html')
 
 
 def stats(request):
-    # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
-    # Чтобы отличить с какой версии лендинга был переход
-    # проверяйте GET параметр marker который может принимать значения test и original
-    # Для вывода результат передайте в следующем формате:
+    if not counter_show['alternative']:
+        counter_show['alternative'] = 1
+    if not counter_show['original']:
+        counter_show['original'] = 1
     return render_to_response('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': format(counter_click['alternative'] / counter_show['alternative'], '.1f'),
+        'original_conversion': format(counter_click['original'] / counter_show['original'], '.1f'),
     })
--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,18 +1,40 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
+from django.conf import settings
+import csv
 
 
 def index(request):
     return redirect(reverse(bus_stations))
 
 
+with open(settings.BUS_STATION_CSV) as f:
+    csv_data = csv.DictReader(f)
+    stations = [row for row in csv_data]
+
+
 def bus_stations(request):
-    current_page = 1
-    next_page_url = 'write your url'
+    main_url = reverse('bus_stations')
+
+    paginator = Paginator([row for row in stations], 10)
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
+    articles = paginator.get_page(current_page)
+    data = articles.object_list
+
+    previous_page_url, next_page_url = None, None
+    if articles.has_previous():
+        previous_page_url = main_url + f'?page={articles.previous_page_number()}'
+    if articles.has_next():
+        next_page_url = main_url + f'?page={articles.next_page_number()}'
+
     return render_to_response('index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'}],
-        'current_page': current_page,
-        'prev_page_url': None,
+        'bus_stations': data,
+        'current_page': articles.number if articles.number < current_page else current_page,
+        'prev_page_url': previous_page_url,
         'next_page_url': next_page_url,
     })
-
